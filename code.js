b4f32ba357a7bd4f2632169ddb920c80fb0b6369
AnaArraiz <70163695+AnaArraiz@users.noreply.github.com>
2020-09-23 13:16:57 -0400

    Add files via upload

--- code.js
+++ code.js
@@ -1,3 +1,4 @@
 ScrollReveal().reveal('.showcase');
 ScrollReveal().reveal('.news-cards', {delay:300});
 ScrollReveal().reveal('.news-desserts', {delay:300});
+ScrollReveal().reveal('.news-healthy-food', {delay:300});
