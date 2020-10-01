644390d468f243da5fb7c823aa18cdbd280590ba
AnaArraiz <70163695+AnaArraiz@users.noreply.github.com>
2020-10-01 16:46:03 -0400

    scroll reveal modificado
    actualizacion del reveal

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
-    ScrollReveal().reveal('.showcase');
-    ScrollReveal().reveal('.info', { delay: 300 });
-    ScrollReveal().reveal('.newscards', { delay: 300 });
-    ScrollReveal().reveal('.news-desserts', { delay: 300 });
-    ScrollReveal().reveal('.news-healthy-food', { delay: 300 });
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.info', { delay: 300 });
+ScrollReveal().reveal('.newscards', { delay: 300 });
+ScrollReveal().reveal('.news-desserts', { delay: 300 });
+ScrollReveal().reveal('.news-healthy-food', { delay: 300 });
